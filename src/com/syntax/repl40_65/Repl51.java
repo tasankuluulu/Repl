e41f45a3f1f83d61c81da2aed8c30ea55e92d54c
tasankuluulu <tasankuluulu@gmail.com>
2020-06-22 08:09:35 -0400

    51 repl added

---
+++ src/com/syntax/repl40_65/Repl51.java
@@ -0,0 +1,13 @@
+package com.syntax.repl40_65;
+
+public class Repl51 {
+	public static void main(String[] args) {
+		int num = 10;
+		do {
+			System.out.println(num);
+			num++;
+		}
+		while (num <= 19);
+	}
+
+}
